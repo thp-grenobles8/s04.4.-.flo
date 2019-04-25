d7317129f06ef278bc01fa4dbc6c2cb658f860e7
Hazrian <chazotflorian@gmail.com>
2019-04-26 00:41:55 +0200

    dependance

--- FreeDoc/app/models/doctor.rb
+++ FreeDoc/app/models/doctor.rb
@@ -1,7 +1,7 @@
 class Doctor < ApplicationRecord
-  has_many :appointments
+  has_many :appointments, dependent: :destroy
   has_many :patients, through: :appointments
-  has_many :join_table_doctor_specialty
+  has_many :join_table_doctor_specialty, dependent: :destroy
   has_many :specialties, through: :join_table_doctor_specialty
   belongs_to :city
 end
--- FreeDoc/app/models/specialty.rb
+++ FreeDoc/app/models/specialty.rb
@@ -1,4 +1,4 @@
 class Specialty < ApplicationRecord
-  has_many :join_table_doctor_specialty
+  has_many :join_table_doctor_specialty, dependent: :destroy
   has_many :doctors, through: :join_table_doctor_specialty
 end
